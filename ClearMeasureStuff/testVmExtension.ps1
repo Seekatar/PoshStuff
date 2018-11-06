5e9b18731ce1b8c1d4609b6f6d4f8dd026dd10f8
Jim W <Seekatar@users.noreply.github.com>
2018-11-06 11:24:27 -0500

    Update testVmExtension.ps1

--- ClearMeasureStuff/testVmExtension.ps1
+++ ClearMeasureStuff/testVmExtension.ps1
@@ -1,1 +1,22 @@
-Set-Content -Path c:\junk.txt -Value "this was written at $(get-date) from $PSScriptRoot"
+# run on the newly minted build box.
+param(
+[Parameter(Mandatory)]
+[sting] $AccountUrl,
+[Parameter(Mandatory)]
+[string] $PAT,
+[string] $Folder = "c:\agent"
+)
+
+mkdir $Folder -ErrorAction SilentlyContinue
+Set-Location $Folder
+
+Add-Content ".\run.log" -Value "$(Get-Date) AccountUrl = $AccountUrl"
+Add-Content ".\run.log" -Value "$(Get-Date) PAT len = $($PAT.Length)"
+
+$fname = (Get-Item vsts-agent*.zip).FullName
+Add-Content ".\run.log" -Value "$(Get-Date) fname is $fname"
+
+Expand-Archive -Path $fname -DestinationPath $Folder
+Add-Content ".\run.log" -Value "$(Get-Date) $fname expanded into $Folder"
+
+.\config.cmd --unattended --url $AccountUrl --auth pat --token $PAT --pool default --agent $env:COMPUTERNAME --runAsService
