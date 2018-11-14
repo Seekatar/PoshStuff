c867563015f82071a8fcda0b7948847aca306a0e
seekatar <seekatar@gmail.com>
2018-11-14 08:44:45 -0500

    Add remove on VsTs

--- ClearMeasureStuff/Add-VstsAgent.ps1
+++ ClearMeasureStuff/Add-VstsAgent.ps1
@@ -54,6 +54,9 @@ Logit -indent "fname is $fname"
 Expand-Archive -Path $fname -DestinationPath $Folder -Force
 Logit -indent "$fname expanded into $Folder"
 
+# ok to remove if not there
+.\config.cmd remove --unattended --auth pat --token $PAT
+
 .\config.cmd --unattended --url $AccountUrl --auth pat --token $PAT --pool $AgentPool --agent $env:COMPUTERNAME --windowsLogonAccount $AdminUser --windowsLogonPassword $AdminUserPwd --runAsService 2>&1 >> $LogFile
 Logit -indent "config.cmd exited and LASTEXITCODE is $LASTEXITCODE" -LastExitCode $LASTEXITCODE
 
